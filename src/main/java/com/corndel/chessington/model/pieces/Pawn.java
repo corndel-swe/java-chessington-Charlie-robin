3f9693eb1a43d5db9f30ecc41f39ac229d794891
Charlie-robin <charlie.robin.richardson@gmail.com>
2024-09-23 16:37:56 +0100

    Workshop Pawn completed

--- src/main/java/com/corndel/chessington/model/pieces/Pawn.java
+++ src/main/java/com/corndel/chessington/model/pieces/Pawn.java
@@ -4,44 +4,60 @@ import com.corndel.chessington.model.Board;
 import com.corndel.chessington.model.Coordinates;
 import com.corndel.chessington.model.Move;
 import com.corndel.chessington.model.PlayerColour;
+
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Pawn implements Piece {
 
-  private final Piece.PieceType type;
-  protected final PlayerColour colour;
+    private final Piece.PieceType type;
+    protected final PlayerColour colour;
 
-  public Pawn(PlayerColour colour) {
-    this.type = PieceType.PAWN;
-    this.colour = colour;
-  }
-
-  @Override
-  public Piece.PieceType getType() {
-    return type;
-  }
-
-  @Override
-  public PlayerColour getColour() {
-    return colour;
-  }
-
-  @Override
-  public String toString() {
-    return colour.toString() + " " + type.toString();
-  }
-
-  @Override
-  public List<Move> getAllowedMoves(Coordinates from, Board board) {
-    var allowedMoves = new ArrayList<Move>();
-    if (getColour().equals(PlayerColour.WHITE)) {
-      allowedMoves.add(new Move(from, from.plus(-1, 0)));
-    } else {
-      allowedMoves.add(new Move(from, from.plus(1, 0)));
+    public Pawn(PlayerColour colour) {
+        this.type = PieceType.PAWN;
+        this.colour = colour;
     }
 
-    // TODO Implement this!
-    return List.of();
-  }
+    @Override
+    public Piece.PieceType getType() {
+        return type;
+    }
+
+    @Override
+    public PlayerColour getColour() {
+        return colour;
+    }
+
+    @Override
+    public String toString() {
+        return colour.toString() + " " + type.toString();
+    }
+
+    @Override
+    public List<Move> getAllowedMoves(Coordinates from, Board board) {
+        List<Move> allowedMoves = new ArrayList<Move>();
+
+        int direction = colour.equals(PlayerColour.WHITE) ? -1 : 1;
+
+        Coordinates next = from.plus(direction, 0);
+
+        Stream.of(next.plus(0, 1), next.plus(0, -1))
+                .filter(c -> board.onBoard(c) && board.canTake(c, colour))
+                .forEach(c -> allowedMoves.add(new Move(from, c)));
+
+        if (!board.onBoard(next) || board.get(next) != null) {
+            return allowedMoves;
+        }
+
+        allowedMoves.add(new Move(from, next));
+
+        if (from.getRow() == 6 || from.getRow() == 1) {
+            allowedMoves.add(new Move(from, from.plus(direction * 2, 0)));
+        }
+
+        return allowedMoves;
+    }
 }
